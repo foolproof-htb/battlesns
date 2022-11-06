8c62e100c3cce84258053cb9e6ff4120b2c0a409
foolproof-htb <fujisawa.htb@gmail.com>
2022-11-06 12:53:45 +0900

    new: package.json を追加し、json-server と接続できるようにした

--- lib/timeline_page.dart
+++ lib/timeline_page.dart
@@ -14,21 +14,10 @@ class TimelinePage extends StatefulWidget {
 class _TimelinePageState extends State<TimelinePage> {
   List<Post> posts = [];
   _fetchPosts() async {
-    // final url = Uri.parse('http://10.0.2.2:3030/posts');
-    // http.Response response = await http.get(url);
-    // List<Map<String, dynamic>> json = jsonDecode(response.body);
+    final url = Uri.parse('http://10.0.2.2:3030/posts');
+    http.Response response = await http.get(url);
+    List<dynamic> json = jsonDecode(response.body);
 
-    // http 接続に失敗するので仮のjsonデータを用意
-    List<Map<String, dynamic>> json = [];
-    for (int i = 1; i <= 10; i++) {
-      json.add({
-        'id': i,
-        'account': '@test$i',
-        'name': 'user$i',
-        'body': 'test message$i',
-        'createdAt': '2022/10/$i 12:25'
-      });
-    }
     setState(() {
       posts = json.map((data) => Post.formJson(data)).toList();
     });
